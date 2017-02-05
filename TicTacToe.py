f0f0c9508d99f77ff4ba70cb664a23086889714b
kandarbh <kandarbhpatel@gmail.com>
2017-02-05 12:27:08 -0800

    Update change by your Leader -Kandarbh Patel

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,3 +1,65 @@
+# Tic-Tac-Toe
+# Plays the game of tic-tac-toe against a human opponent
+
+# global constants
+X = "X"
+O = "O"
+EMPTY = " "
+TIE = "TIE"
+NUM_SQUARES = 9
+
+
+def display_instruct():
+    """Display game instructions."""
+    print(
+    """
+    Welcome to the greatest intellectual challenge of all time: Tic-Tac-Toe.
+    This will be a showdown between your human brain and my silicon processor.
+
+    You will make your move known by entering a number, 0 - 8.  The number
+    will correspond to the board position as illustrated:
+
+                    0 | 1 | 2
+                    ---------
+                    3 | 4 | 5
+                    ---------
+                    6 | 7 | 8
+
+    Prepare yourself, human.  The ultimate battle is about to begin. \n
+    """
+    )
+
+
+def ask_yes_no(question):
+    """Ask a yes or no question."""
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
+
+
+def ask_number(question, low, high):
+    """Ask for a number within a range."""
+    response = None
+    while response not in range(low, high):
+        response = int(input(question))
+    return response
+
+
+def pieces():
+    """Determine if player or computer goes first."""
+    go_first = ask_yes_no("Do you require the first move? (y/n): ")
+    if go_first == "y":
+        print("\nThen take the first move.  You will need it.")
+        human = X
+        computer = O
+    else:
+        print("\nYour bravery will be your undoing... I will go first.")
+        computer = X
+        human = O
+    return computer, human
+
+
 def new_board():
     """Create new game board."""
     board = []
@@ -47,7 +109,7 @@ def winner(board):
 
 
 def human_move(board, human):
-    """Get human move."""  
+    """Get human move."""
     legal = legal_moves(board)
     move = None
     while move not in legal:
